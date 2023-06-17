d30003b148be9146a3001154fd94a852e6174d32
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-06-17 12:28:01 -0400

    Update robot-config.cpp

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -4,7 +4,7 @@ using namespace vex;
 
 // A global instance of brain used for printing to the V5 brain screen
 brain Brain;
-
+controller gameController = controller(primary);
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  *
