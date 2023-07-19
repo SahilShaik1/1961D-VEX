ce829c82080fdc69e61a9bd79926cf7a0617eb55
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-07-19 18:11:09 -0400

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -2,6 +2,14 @@ using namespace vex;
 
 extern brain Brain;
 extern controller gameController;
+extern motor rightUP;
+extern motor rightDN;
+extern motor rightSN;
+extern motor leftUP;
+extern motor leftDN;
+extern motor leftSN;
+extern motor_group rightMotors;
+extern motor_group leftMotors;
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  *
