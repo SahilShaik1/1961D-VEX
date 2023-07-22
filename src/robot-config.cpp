ee0f0e8ffc08b33e8d19fe72df365f3327642877
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-07-21 21:33:20 -0400

    limit switches/slingshot addition

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -5,6 +5,8 @@ using namespace vex;
 // A global instance of brain used for printing to the V5 brain screen
 brain Brain;
 controller gameController = controller(primary);
+triport tp(PORT22);
+limit limitSwitch = limit(tp.A);
 motor rightUP = motor(PORT1, ratio18_1, false);
 motor rightDN = motor(PORT2, ratio18_1, true);
 motor rightSN = motor(PORT3, ratio18_1, false);
@@ -12,6 +14,7 @@ motor leftUP  = motor(PORT4, ratio18_1, false);
 motor leftDN  = motor(PORT5, ratio18_1, true);
 motor leftSN = motor(PORT6, ratio18_1, false);
 motor intake = motor(PORT7, ratio18_1, false);
+motor coilMotor = motor(PORT8, ratio18_1, false);
 motor_group rightMotors = motor_group(rightUP, rightDN, rightSN);
 motor_group leftMotors = motor_group(leftUP, leftDN, leftSN);
 /**
