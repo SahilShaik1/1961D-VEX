76182f9a5887d06188145b9a1d9c964b9d51ef0e
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-09-02 22:04:20 -0400

    Update robot-config.cpp

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -1,27 +1,98 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
+// A global instance of brain used for printing to the V5 Brain screen
 brain Brain;
-controller gameController = controller(primary);
 triport tp(PORT22);
 limit limitSwitch = limit(tp.A);
-motor rightUP = motor(PORT1, ratio18_1, false);
-motor rightDN = motor(PORT2, ratio18_1, true);
-motor rightSN = motor(PORT3, ratio18_1, false);
-motor leftUP  = motor(PORT4, ratio18_1, false);
-motor leftDN  = motor(PORT5, ratio18_1, true);
-motor leftSN = motor(PORT6, ratio18_1, false);
-motor intake = motor(PORT7, ratio18_1, false);
-motor coilMotor = motor(PORT8, ratio18_1, false);
-motor_group rightMotors = motor_group(rightUP, rightDN, rightSN);
-motor_group leftMotors = motor_group(leftUP, leftDN, leftSN);
+// VEXcode device constructors
+motor leftMotorA = motor(PORT11, ratio18_1, false);
+motor leftMotorB = motor(PORT12, ratio18_1, false);
+motor leftMotorC = motor(PORT13, ratio18_1, true);
+motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB, leftMotorC);
+motor rightMotorA = motor(PORT18, ratio18_1, true);
+motor rightMotorB = motor(PORT19, ratio18_1, true);
+motor rightMotorC = motor(PORT20, ratio18_1, false);
+
+motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB, rightMotorC);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+controller Controller1 = controller(primary);
+
+
+
+motor intake = motor(PORT1, ratio18_1, false);
+motor coilMotor = motor(PORT1, ratio18_1, false);
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3 + Axis1
+      // right = Axis3 - Axis1
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position() + Controller1.Axis1.position();
+      int drivetrainRightSideSpeed = Controller1.Axis3.position() - Controller1.Axis1.position();
+      
+      // check if the value is inside of the deadband range
+      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
+        // check if the left motor has already been stopped
+        if (DrivetrainLNeedsToBeStopped_Controller1) {
+          // stop the left drive motor
+          LeftDriveSmart.stop();
+          // tell the code that the left motor has been stopped
+          DrivetrainLNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
+        DrivetrainLNeedsToBeStopped_Controller1 = true;
+      }
+      // check if the value is inside of the deadband range
+      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
+        // check if the right motor has already been stopped
+        if (DrivetrainRNeedsToBeStopped_Controller1) {
+          // stop the right drive motor
+          RightDriveSmart.stop();
+          // tell the code that the right motor has been stopped
+          DrivetrainRNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
+        DrivetrainRNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainLNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainRNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
-  // Nothing to initialize
+void vexcodeInit( void ) {
+  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
 }
