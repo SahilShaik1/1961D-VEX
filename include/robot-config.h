ad7aa5a7915b79186005b5304eeaf811ab901002
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-12-11 18:53:36 -0700

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -8,13 +8,14 @@ extern controller Controller1;
 extern motor leftMotorA;
 extern motor rightMotorA; 
 extern motor intake;
-extern motor cata;
+extern motor lcata;
+extern motor rcata;
 extern motor_group LeftDriveSmart;
 extern motor_group RightDriveSmart;
 extern inertial imu1;
 /**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
- * This should be called at the start of your int main function.
- */
+ 
+Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+This should be called at the start of your int main function.
+*/
 void  vexcodeInit( void );
